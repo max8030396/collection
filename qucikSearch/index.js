46e3033df9e24418fc2e72c66ed2c940005876a4
max <max8030396@gmail.com>
2021-02-10 14:41:08 +0800

    no message

--- qucikSearch/index.js
+++ qucikSearch/index.js
@@ -1,7 +1,19 @@
 $(document).ready(() => {
+  const PTT = $('.s2_nav1')
+  const Google = $('.s2_nav2')
+  const Yahoo = $('.s2_nav3')
+  const s2_block_nav = $('.s2_block_nav');
+  
 
-
-
+  function navClickSwitch(){
+    s2_block_nav.on('click', function(){
+      $(this).addClass('js_active').siblings().removeClass('js_active')
+    })
+  }
 
   
-}
+
+
+
+  navClickSwitch();
+})
