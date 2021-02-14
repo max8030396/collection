d551414526ce7b829817b8c8979a0f7b7d556bb2
max <max8030396@gmail.com>
2021-02-14 14:20:53 +0800

    2/14 H1顏色 框架之間的間距 文字間距 文字超過範圍隱藏 RWD 骨架間距調整 刪除H1新聞列表

--- quickSearch/index.js
+++ quickSearch/index.js
@@ -1,19 +1,13 @@
 $(document).ready(() => {
-  const PTT = $('.s2_nav1')
-  const Google = $('.s2_nav2')
-  const Yahoo = $('.s2_nav3')
   const s2_block_nav = $('.s2_block_nav');
   
-
-  function navClickSwitch(){
+  function navChoose(){
     s2_block_nav.on('click', function(){
-      $(this).addClass('js_active').siblings().removeClass('js_active')
+      // $(this).addClass('js_active').siblings().removeClass('js_active') IE舊版本不支援
+      s2_block_nav.removeClass('js_active');
+      $(this).addClass('js_active');
     })
   }
 
-  
-
-
-
-  navClickSwitch();
+  navChoose();
 })
