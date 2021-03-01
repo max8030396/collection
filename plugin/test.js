dd036feb70daced53ead2e8ec5169e718e0cd905
max <max8030396@gmail.com>
2021-03-01 12:28:07 +0800

    2/25 小幅修正外觀、ＲＷＤ 新增能量條 更改顏色

--- plugin/test.js
+++ plugin/test.js
@@ -9,9 +9,6 @@ $(document).ready(function() {
     bgColor: '#f00',
   })
 
-
-
-  
   var words = [
     {text: "台積電", weight: 13, link: 'http://github.com/mistic100/jQCloud'},
     {text: "聯發科", weight: 10.5, link: 'http://www.strangeplanet.fr'},
@@ -30,4 +27,65 @@ $(document).ready(function() {
     autoResize: true,
     colors: ["#800026", "#bd0026", "#e31a1c", "#fc4e2a", "#fd8d3c", "#d0da", "#00a"],
     });
+
+  const myData = [
+    {key:'num1', value: 100 },
+    {key:'num2', value: 10 },
+    {key:'num3', value: 87 },
+    {key:'num4', value: 40 },
+    {key:'num5', value: 35 },
+    {key:'num6', value: 23 }
+    // {key:自定義, value: 數值 }
+  ]
+
+  $('#barChart').simpleBarGraph({
+      data: myData,
+      rowsCount: 5,
+      barsColor:'#fa9',
+      rowCaptionsWidth:'16px',
+      height:'400px',
+      // delay<a href="https://www.jqueryscript.net/animation/">Animation</a>: 10
+    });
+  
+  var ctx = document.getElementById('barChar');
+  var barChar = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['black', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange' , 'black'],//圖表數量及名稱
+        datasets: [{
+          label: '圖表1',//圖表上方說明
+          data: [5, 19, 3, 5, 2, 3 ,99],//數值
+          backgroundColor: [
+              'rgba(255, 99, 132)',//主顏色
+              'rgba(54, 162, 235)',
+              'rgba(255, 206, 86)',
+              'rgba(75, 192, 192)',
+              'rgba(153, 102, 255)',
+              'rgba(255, 159, 64)'
+          ],
+          borderColor: [
+              'rgba(000, 0, 132, 1)',//外框顏色
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 0//外框粗度
+      }]
+      },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+  });
+  
+  
+
+
 })
--- quickSearch/index.js
+++ quickSearch/index.js
@@ -9,26 +9,26 @@ $(document).ready(() => {
     })
   }
   var words = [
-    {text: "台積電", weight: 13, link: 'http://github.com/mistic100/jQCloud'},
-    {text: "聯發科", weight: 10.5, link: 'http://www.strangeplanet.fr'},
+    {text: "台積電", weight: 11.5, link: 'http://github.com/mistic100/jQCloud'},
+    {text: "聯發科", weight: 8.5, link: 'http://www.strangeplanet.fr'},
     {text: "中鋼", weight: 9.4, link: 'http://piwigo.org'},
-    {text: "富邦金", weight: 13},
-    {text: "玉山金", weight: 10.5},
-    {text: "中華", weight: 9.4},
-    {text: "遠傳", weight: 8},
-    {text: "台哥大", weight: 6.2},
-    {text: "華碩", weight: 5},
-    {text: "宏碁", weight: 5},
+    {text: "富邦金", weight: 13, link: 'http://piwigo.org'},
+    {text: "玉山金", weight: 7.5, link: 'http://piwigo.org'},
+    {text: "中華", weight: 9.4, link: 'http://piwigo.org'},
+    {text: "遠傳", weight: 8, link: 'http://piwigo.org'},
+    {text: "台哥大", weight: 6.2, link: 'http://piwigo.org'},
+    {text: "華碩", weight: 16.9, link: 'http://piwigo.org'},
+    {text: "宏碁", weight: 11.3, link: 'http://piwigo.org'},
     /* ... */
   ];
 
-  $('#cloud1').jQCloud(words, {
+  $('#s4_block_left_cloud1').jQCloud(words, {
     autoResize: true,
-    colors: ["#800026", "#bd0026", "#e31a1c", "#fc4e2a", "#fd8d3c", "#d0da", "#00a"],
+    colors: ["#e7228c", "#e7237c", "#e01671", "#dd0f6c", "#d90866", "#b5179e", "#a514a5", "#9410ab", "#8c0fae", "#830db1"],
     });
-  $('#cloud2').jQCloud(words, {
-    autoResize: true,
-    colors: ["#800026", "#bd0026", "#e31a1c", "#fc4e2a", "#fd8d3c", "#d0da", "#00a"],
+  $('#s4_block_right_cloud2').jQCloud(words, {
+    // autoResize: true,
+    colors: ["#800026","#6c757d", "#bd0026", "#00a", "#e31a1c", "#5E503F", "#fc4e2a", "#fd8d3c", "#0D6FB8", "#1b4332"],
     });
 
 
@@ -41,7 +41,7 @@ $(document).ready(() => {
       async: true,
       type: "GET",
       beforeSend: function() {
-        console.log('beforeWhenSend');
+        console.log('beforeSend');
       },
       success: function(res) {
         console.log('success' , res.news[0]);
@@ -59,7 +59,105 @@ $(document).ready(() => {
     }
     });
   }
-  
-navChoose();
-handleApiAjax();
+
+  function barChart1() {
+    var ctx = document.getElementById('s4_block_left_barChart1');
+    var s4_block_left_barChart1 = new Chart(ctx, {
+      type: 'bar',
+      data: {
+          labels: ['利益', '成長', '效能', '高效', '高效能', '帶動' , '穩定', '穩定的', '人傑' , '上升'],//圖表數量及名稱
+          datasets: [{
+            label: '',//圖表上方說明
+            data: [5, 19, 3, 5, 2, 3 , 9, 26, 12, 17],//數值
+            backgroundColor: [
+                'rgba(45, 0, 247, 0.7)',//主顏色
+                'rgba(106, 0, 244, 0.7)',
+                'rgba(137, 0, 242, 0.7)',
+                'rgba(161, 0, 242, 0.7)',
+                'rgba(177, 0, 232, 0.7)',
+                'rgba(188, 0, 221, 0.7)',
+                'rgba(209, 0, 209, 0.7)',
+                'rgba(219, 0, 182, 0.7)',
+                'rgba(229, 0, 164, 0.7)',
+                'rgba(242, 0, 137, 0.7)'
+            ],
+            borderColor: [
+                'rgba(45, 0, 247, 1)',//外框顏色
+                'rgba(106, 0, 244, 1)',
+                'rgba(137, 0, 242, 1)',
+                'rgba(161, 0, 242, 1)',
+                'rgba(177, 0, 232, 1)',
+                'rgba(188, 0, 221, 1)',
+                'rgba(209, 0, 209, 1)',
+                'rgba(219, 0, 182, 1)',
+                'rgba(229, 0, 164, 1)',
+                'rgba(242, 0, 137, 1)'
+            ],
+            borderWidth: 1.5//外框粗度
+        }]
+        },
+      options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero: true
+                  }
+              }]
+          }
+      }
+    });
+  }
+
+  function barChart2() {
+    var ctx = document.getElementById('s4_block_right_barChart2');
+    var s4_block_right_barChart2 = new Chart(ctx, {
+      type: 'bar',
+      data: {
+          labels: ['利益', '成長', '效能', '高效', '高效能', '帶動' , '穩定', '穩定的', '人傑' , '上升'],//圖表數量及名稱
+          datasets: [{
+            label: '',//圖表上方說明
+            data: [5, 19, 3, 5, 2, 3 , 9, 26, 12, 17],//數值
+            backgroundColor: [
+                'rgba(45, 0, 247, 0.7)',//主顏色
+                'rgba(106, 0, 244, 0.7)',
+                'rgba(137, 0, 242, 0.7)',
+                'rgba(161, 0, 242, 0.7)',
+                'rgba(177, 0, 232, 0.7)',
+                'rgba(188, 0, 221, 0.7)',
+                'rgba(209, 0, 209, 0.7)',
+                'rgba(219, 0, 182, 0.7)',
+                'rgba(229, 0, 164, 0.7)',
+                'rgba(242, 0, 137, 0.7)'
+            ],
+            borderColor: [
+                'rgba(45, 0, 247, 1)',//外框顏色
+                'rgba(106, 0, 244, 1)',
+                'rgba(137, 0, 242, 1)',
+                'rgba(161, 0, 242, 1)',
+                'rgba(177, 0, 232, 1)',
+                'rgba(188, 0, 221, 1)',
+                'rgba(209, 0, 209, 1)',
+                'rgba(219, 0, 182, 1)',
+                'rgba(229, 0, 164, 1)',
+                'rgba(242, 0, 137, 1)'
+            ],
+            borderWidth: 1.5//外框粗度
+        }]
+        },
+      options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero: true
+                  }
+              }]
+          }
+      }
+    });
+  }
+
+  navChoose();
+  handleApiAjax();
+  barChart1();
+  barChart2();
 })
