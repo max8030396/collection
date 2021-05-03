2d352b3c67a300459d3c17b45feb21a39b095650
max <max8030396@gmail.com>
2021-05-03 19:59:27 +0800

    no message

--- 外包/starklab/stark_lab/frontend/build/js/watchCenter.js
+++ 外包/starklab/stark_lab/frontend/build/js/watchCenter.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f1d0428304698e451119";
+/******/ 	var hotCurrentHash = "0b780edc8ea505ae4fd0";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
